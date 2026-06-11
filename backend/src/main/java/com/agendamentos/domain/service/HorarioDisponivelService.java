fc9878055be7d580066ad8661c6b32a39c60106f
Vinícius souza <vinicanal604@gmail.com>
2026-06-10 22:41:46 -0300

    fix: Arrumando imports

--- backend/src/main/java/com/agendamentos/domain/service/HorarioDisponivelService.java
+++ backend/src/main/java/com/agendamentos/domain/service/HorarioDisponivelService.java
@@ -7,6 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.UUID;
 
